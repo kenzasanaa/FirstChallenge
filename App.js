1810a85ee4c64aa47bd51b22dc88cd2042225d69
shegoo <kessi.k@edu.spbstu.ru>
2025-01-28 22:44:40 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import {useState } from "react";
+
+export default function App() {
+  return <div>
+    <Steps />
+  </div>
+}
+
+function Steps() { 
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("january 28 2025");
+  date.setDate(date.getDate() + count);
+  return <div>
+    <div>
+      <button onClick={ () => setStep((c) => c-1)}>-</button>
+      <span>step: {step}</span>
+      <button onClick={ () => setStep((c) => c+1)}>+</button>
+    </div>
+    <div>
+      <button onClick={ () => setCount((c) => c- step)}>-</button>
+      <span>count: {count}</span>
+      <button onClick={ () => setCount((c) => c+ step)}>+</button>
+    </div>
+    <span> {count === 0? "Today is " : count > 0 ? `${count} days from today is` : `${Math.abs(count)} days ago was `}</span>
+    <span> {date.toDateString()}</span>
+  </div>
+}
